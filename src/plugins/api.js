d048ee0f315559ec48bd220448017cc6070dd4cb
Gabriel Lucas <126280066+Gabz047@users.noreply.github.com>
2025-06-12 16:21:02 -0300

    Update api.js

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //Allows the application to make requisition with the backend
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json"
     }
